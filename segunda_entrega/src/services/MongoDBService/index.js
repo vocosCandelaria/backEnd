1e8407e466f9ff5f82d84195085c52d60e5eb06f
vocosCandelaria <vocoscandelaria@gmail.com>
2022-11-22 21:57:03 -0300

    segunda_entrega

---
+++ segunda_entrega/src/services/MongoDBService/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { config } from "../../config/index.js";
+
+const init = async () => {
+  try {
+    mongoose.connect(config.DATABASES.mongo.url, {
+      dbName: config.DATABASES.mongo.dbName,
+    });
+    console.log("✔ Connection with mongodb established");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const MongoDBService = {
+  init,
+};
